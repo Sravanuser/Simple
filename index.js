63578834f5545d71d4bc63e95c48257bc2943570
Sravan Kumar <sravanpatel0017@gmail.com>
2025-06-10 23:57:04 +0530

    first commit

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use("/api", UserRoute);
 app.use(express.static(path.join(dirname, "/client/dist")))
 
-app.get("*", (request, response) => {
+app.get("/{*any}", (request, response) => {
     response.sendFile(path.join(dirname, "client", "dist", "index.html"))
 })
 
